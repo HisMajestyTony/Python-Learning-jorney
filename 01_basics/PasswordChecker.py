28c8c941bc2d70263550ce448291e80f98a3d7b8
Antonio Tsanov <antonio.du.tsanov@gmail.com>
2026-02-15 08:01:23 -0600

    Added some Matrix Exmples

---
+++ 01_basics/PasswordChecker.py
@@ -0,0 +1,8 @@
+username = input("Username: ")
+password = input("Password")
+
+passwordLenght = len(password)
+pass_security = '*' * passwordLenght
+
+
+print(f'{username}, your password {pass_security} is {passwordLenght} letters long')
